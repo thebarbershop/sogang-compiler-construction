ca929f94caa8fdf42aa326e7697b1bfdc422df20
taegyung <taegyung@sogang.ac.kr>
2019-03-26 04:02:37 +0900

    auto formatting and token update

--- project1/globals.h
+++ project1/globals.h
@@ -1,9 +1,10 @@
 /****************************************************/
 /* File: globals.h                                  */
-/* Global types and vars for TINY compiler          */
+/* Global types and vars for C- compiler            */
 /* must come before other include files             */
 /* Compiler Construction: Principles and Practice   */
-/* Kenneth C. Louden                                */
+/* Kenneth C. Louden, 1997                          */
+/* Modified by Eom Taegyung                         */
 /****************************************************/
 
 #ifndef _GLOBALS_H_
@@ -23,22 +24,49 @@
 #endif
 
 /* MAXRESERVED = the number of reserved words */
-#define MAXRESERVED 8
+#define MAXRESERVED 6
 
-typedef enum 
-    /* book-keeping tokens */
-   {ENDFILE,ERROR,
-    /* reserved words */
-    IF,THEN,ELSE,END,REPEAT,UNTIL,READ,WRITE,
-    /* multicharacter tokens */
-    ID,NUM,
-    /* special symbols */
-    ASSIGN,EQ,LT,PLUS,MINUS,TIMES,OVER,LPAREN,RPAREN,SEMI
-   } TokenType;
+/* Match the order of cmin.l */
+typedef enum
+/* book-keeping tokens */
+{
+   ENDFILE,
+   ERROR,
+   /* reserved words */
+   ELSE,
+   IF,
+   INT,
+   RETURN,
+   VOID,
+   WHILE,
+   /* special symbols */
+   PLUS,
+   MINUS,
+   TIMES,
+   OVER,
+   LT,
+   LTE,
+   GT,
+   GTE,
+   EQ,
+   NEQ,
+   ASSIGN,
+   SEMI,
+   COMMA,
+   LPAREN,
+   RPAREN,
+   LBRACKET,
+   RBRACKET,
+   LBRACE,
+   RBRACE,
+   /* multicharacter tokens */
+   ID,
+   NUM
+} TokenType;
 
-extern FILE* source; /* source code text file */
-extern FILE* listing; /* listing output text file */
-extern FILE* code; /* code text file for TM simulator */
+extern FILE *source;  /* source code text file */
+extern FILE *listing; /* listing output text file */
+extern FILE *code;    /* code text file for TM simulator */
 
 extern int lineno; /* source line number for listing */
 
@@ -46,26 +74,53 @@ extern int lineno; /* source line number for listing */
 /***********   Syntax tree for parsing ************/
 /**************************************************/
 
-typedef enum {StmtK,ExpK} NodeKind;
-typedef enum {IfK,RepeatK,AssignK,ReadK,WriteK} StmtKind;
-typedef enum {OpK,ConstK,IdK} ExpKind;
+typedef enum
+{
+   StmtK,
+   ExpK
+} NodeKind;
+typedef enum
+{
+   IfK,
+   RepeatK,
+   AssignK,
+   ReadK,
+   WriteK
+} StmtKind;
+typedef enum
+{
+   OpK,
+   ConstK,
+   IdK
+} ExpKind;
 
 /* ExpType is used for type checking */
-typedef enum {Void,Integer,Boolean} ExpType;
+typedef enum
+{
+   Void,
+   Integer,
+   Boolean
+} ExpType;
 
 #define MAXCHILDREN 3
 
 typedef struct treeNode
-   { struct treeNode * child[MAXCHILDREN];
-     struct treeNode * sibling;
-     int lineno;
-     NodeKind nodekind;
-     union { StmtKind stmt; ExpKind exp;} kind;
-     union { TokenType op;
-             int val;
-             char * name; } attr;
-     ExpType type; /* for type checking of exps */
-   } TreeNode;
+{
+   struct treeNode *child[MAXCHILDREN];
+   struct treeNode *sibling;
+   int lineno;
+   NodeKind nodekind;
+   union {
+      StmtKind stmt;
+      ExpKind exp;
+   } kind;
+   union {
+      TokenType op;
+      int val;
+      char *name;
+   } attr;
+   ExpType type; /* for type checking of exps */
+} TreeNode;
 
 /**************************************************/
 /***********   Flags for tracing       ************/
@@ -100,5 +155,5 @@ extern int TraceAnalyze;
 extern int TraceCode;
 
 /* Error = TRUE prevents further passes if an error occurs */
-extern int Error; 
+extern int Error;
 #endif
