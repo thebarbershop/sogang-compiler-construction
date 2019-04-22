a70eb5c96a486fe8ed744430968ccd47d4ee6cda
taegyung <taegyung@sogang.ac.kr>
2019-04-22 14:40:36 +0900

    add scan.h for project2

---
+++ project2/scan.h
@@ -0,0 +1,23 @@
+/****************************************************/
+/* File: scan.h                                     */
+/* The scanner interface for the C- compiler        */
+/* Compiler Construction: Principles and Practice   */
+/* Kenneth C. Louden, 1997                          */
+/* Modified by Eom Taegyung                         */
+/****************************************************/
+
+#ifndef _SCAN_H_
+#define _SCAN_H_
+
+/* MAXTOKENLEN is the maximum size of a token */
+#define MAXTOKENLEN 40
+
+/* tokenString array stores the lexeme of each token */
+extern char tokenString[MAXTOKENLEN + 1];
+
+/* function getToken returns the 
+ * next token in source file
+ */
+TokenType getToken(void);
+
+#endif
