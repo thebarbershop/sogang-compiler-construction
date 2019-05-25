9ed32661357f87c8fa602bece854dba6c7f7a798
taegyung <taegyung@sogang.ac.kr>
2019-05-25 18:10:52 +0900

    add files for for project 3

---
+++ src/analyze.h
@@ -0,0 +1,22 @@
+/****************************************************/
+/* File: analyze.h                                  */
+/* Semantic analyzer interface for C- compiler      */
+/* Compiler Construction: Principles and Practice   */
+/* Kenneth C. Louden, 1997                          */
+/* Modified by Eom Taegyung                         */
+/****************************************************/
+
+#ifndef _ANALYZE_H_
+#define _ANALYZE_H_
+
+/* Function buildSymtab constructs the symbol 
+ * table by preorder traversal of the syntax tree
+ */
+void buildSymtab(TreeNode *);
+
+/* Procedure typeCheck performs type checking 
+ * by a postorder syntax tree traversal
+ */
+void typeCheck(TreeNode *);
+
+#endif
